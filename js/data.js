463b2136d15a90b5002dc420e822f9f474efcf87
Danil Gurin <gooreen.frontend@gmail.com>
2023-10-19 20:03:43 +0500

    fix mistakes, add new function

--- js/data.js
+++ js/data.js
@@ -1,7 +1,3 @@
-import {getRandomInteger, getRandomArrayElement} from './util.js';
-
-// ---- DATA ----
-
 const PHOTOS_QUANTITY = 25;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
@@ -73,29 +69,13 @@ const USERS__NAMES = [
   'Роман'
 ];
 
-// ---- FUNCTIONS ----
-
-const createComment = (index) => ({
-  id : index,
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message : getRandomArrayElement(COMMENTS__MESSAGES),
-  name : getRandomArrayElement(USERS__NAMES)
-});
-
-const createPhoto = (index) => ({
-  id : index,
-  url : `photos/${index}.jpg`,
-  description: `${PHOTOS_DESCRIPTIONS[index - 1]}`,
-  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-  comments : Array.from(
-    {length : getRandomInteger(MIN_COMMENTS, MAX_COMMENTS)},
-    (_, commentIndex) => createComment(commentIndex + 1)
-  ),
-});
-
-const getPictures = () =>
-  Array.from({ length: PHOTOS_QUANTITY }, (_, pictureIndex) =>
-    createPhoto(pictureIndex + 1)
-  );
-
-export {getPictures};
+export {
+  PHOTOS_QUANTITY,
+  MIN_LIKES,
+  MAX_LIKES,
+  MIN_COMMENTS,
+  MAX_COMMENTS,
+  PHOTOS_DESCRIPTIONS,
+  COMMENTS__MESSAGES,
+  USERS__NAMES
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-import {getPictures} from './data.js';
+import {getPictures} from './data-generation.js';
+
 
 getPictures();
