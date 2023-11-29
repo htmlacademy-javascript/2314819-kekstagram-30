f7a260099a4b396941dce38793838ea6502f595e
Danil Gurin <gooreen.frontend@gmail.com>
2023-11-29 20:46:08 +0500

    final

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-// import { getPictures } from './create-photos-array.js';
 import { renderThumbnails } from './thumbnail-generation.js';
 import './form.js';
 import { loadPicture } from './api.js';
 import { showErrorMessage } from './utils.js';
+import { initFilter } from './filter.js';
 
 const bootstrap = async() => {
   try {
     const pictures = await loadPicture();
     renderThumbnails(pictures);
+    initFilter(pictures);
   } catch (error) {
     showErrorMessage();
   }
