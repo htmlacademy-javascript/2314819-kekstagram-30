421308d3eca31026f38a6bcb547c4ff7f9dc6ec7
Danil Gurin <gooreen.frontend@gmail.com>
2023-11-30 18:26:39 +0500

    fix errors 3

--- js/util.js
+++ js/util.js
@@ -6,8 +6,6 @@ const dataErrorMessageTemplate = document.querySelector('#data-error')
 
 const isEscape = (evt) => evt.key === 'Escape';
 
-// Ошибка загрузки файлов с сервера
-
 const showDataErrorMessage = () => {
   const dataErrorMessage = dataErrorMessageTemplate.cloneNode(true);
   document.body.appendChild(dataErrorMessage);
